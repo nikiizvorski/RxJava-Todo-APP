dc001bbcbfef31d80f55be9edd86767f24054670
NIKI <nikiizvorski@gmail.com>
2016-08-05 09:28:02 +0100

    initial commit

---
+++ app/src/main/java/com/treehouse/android/rxjavaworkshop/FilterPositions.java
@@ -0,0 +1,12 @@
+package com.treehouse.android.rxjavaworkshop;
+
+/**
+ * Created by NIKI on 8/4/2016.
+ */
+public interface FilterPositions {
+
+    //public static final int ALL = 0;
+    public static final int INCOMPLETE = 1;
+    public static final int COMPLETE = 2;
+
+}
