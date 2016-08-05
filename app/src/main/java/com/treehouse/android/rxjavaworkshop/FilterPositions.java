5e11daa1ad593c3cba0f5a61a1d49eadd42e3d93
NIKI <nikiizvorski@gmail.com>
2016-08-05 10:50:58 +0100

    Refactor Butterknife and more!

--- app/src/main/java/com/treehouse/android/rxjavaworkshop/FilterPositions.java
+++ app/src/main/java/com/treehouse/android/rxjavaworkshop/FilterPositions.java
@@ -5,7 +5,7 @@ package com.treehouse.android.rxjavaworkshop;
  */
 public interface FilterPositions {
 
-    //public static final int ALL = 0;
+    public static final int ALL = 0;
     public static final int INCOMPLETE = 1;
     public static final int COMPLETE = 2;
 
--- app/src/main/java/com/treehouse/android/rxjavaworkshop/MainActivity.java
+++ app/src/main/java/com/treehouse/android/rxjavaworkshop/MainActivity.java
@@ -22,6 +22,8 @@ import com.jakewharton.rxbinding.widget.RxAdapterView;
 
 import java.util.List;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.functions.Func2;
@@ -30,17 +32,19 @@ import rx.subscriptions.CompositeSubscription;
 public class MainActivity extends AppCompatActivity {
 
     private static final String LIST = "list";
+    private static final String FILTER = "filter";
 
-
+    @BindView(R.id.toolbar)
     Toolbar toolbar;
+    @BindView(R.id.spinner)
     Spinner spinner;
-
+    @BindView(R.id.add_todo_input)
     EditText addInput;
-
+    @BindView(R.id.recyclerview)
     RecyclerView recyclerView;
-    TodoAdapter adapter;
 
     TodoList list;
+    int filterPosition = FilterPositions.ALL;
 
     CompositeSubscription subscriptions = new CompositeSubscription();
 
@@ -50,37 +54,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Bind Butterknife
+        ButterKnife.bind(this);
+
+        //setup toolbar
+        setSupportActionBar(toolbar);
+        //setTitle("RxJava");
+
         // retrieve from Saved State or SharedPreferences if we are starting fresh
         if (savedInstanceState != null) {
+            //get SavedInstance from Key LIST
             list = new TodoList(savedInstanceState.getString(LIST));
+            //get SavedInstance from Key Filter
+            filterPosition = savedInstanceState.getInt(FILTER, FilterPositions.ALL);
         } else {
             list = new TodoList(getSharedPreferences("data", Context.MODE_PRIVATE).getString(LIST, null));
+            list.add(new Todo("Sample 1", true));
+            list.add(new Todo("Sample 2", false));
+            list.add(new Todo("Sample 3", false));
+            list.add(new Todo("Sample 4", true));
         }
 
-        // setup the Filter which allows us to get the data we want to display
-        //filter = new TodoListFilter(list);
-
         // setup the Adapter, this contains a callback when an item is checked/unchecked
-        adapter = new TodoAdapter(this, list);
-
-        //Adding Observer!
-        //And adding adapter to the observer to notify changes.
-        //list.asObservable().subscribe(adapter);
-        //list.asObservable().subscribe(filter);
-        //we dont need these anymore we combined them in to Observer Combine
+        TodoAdapter adapter = new TodoAdapter(this, list);
 
         // setup the list with the adapter
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerview);
         recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
         recyclerView.setAdapter(adapter);
 
-        // setup adding new items to the list
-        addInput = (EditText) findViewById(R.id.add_todo_input);
         findViewById(R.id.add_todo_container).requestFocus(); // ensures the edittext isn't focused when entering the Activity
 
-
         //Add all subscriptions to CompositeSub
-
         subscriptions.add(
         RxView.clicks(findViewById(R.id.add_todo_input))
                 .map(new Func1<Void, String>() {
@@ -110,13 +114,6 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }));
 
-        // setup the toolbar
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // setup the filter in the toolbar
-        spinner = (Spinner) toolbar.findViewById(R.id.spinner);
-        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"All", "Incomplete", "Completed"}));
         //We just want to update the adapter on changes we dont care about the items thats why we skip the
         //first element.
         //Add Subscript to CompositeSub
@@ -138,11 +135,14 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         ).subscribe(adapter));
+        spinner.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new String[]{"All", "Incomplete", "Completed"}));
+        spinner.setSelection(filterPosition);
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         outState.putString(LIST, list.toString());
+        outState.putInt(FILTER, spinner.getSelectedItemPosition());
         super.onSaveInstanceState(outState);
     }
 
@@ -150,7 +150,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        //unsubscribing all the subscriptions! :)
+        //unsubscribe all the subscriptions! :)
         subscriptions.unsubscribe();
         SharedPreferences.Editor editor = getSharedPreferences("data", Context.MODE_PRIVATE).edit();
         editor.putString(LIST, list.toString());
